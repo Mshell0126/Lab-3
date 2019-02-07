af7e0c326afd13f2549cbd04f8377dc3a441b038
Marcus Shell <mshell2@stu.jsu.edu>
2019-02-07 15:14:23 -0600

    initial commit
    most tasks completed
    
    need to create implementation for handling end of game or resetting game.

---
+++ app/src/main/java/com/example/jsu/lab3/CryptoLogic.java
@@ -0,0 +1,125 @@
+package com.example.jsu.lab3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import android.widget.*;
+
+public class CryptoLogic extends AppCompatActivity {
+
+    private Random random = new Random();
+
+    private ArrayList<String> secretWords = new ArrayList(Arrays.asList("APPLE", "BANANA", "CHERRY"));
+    private String currentWord;
+    private String guessBuilder;
+    private int guessCounter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto_logic);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        setNextWord();
+
+
+    }
+
+    public void onGuess(View v){
+
+            EditText inputBox = (EditText) findViewById(R.id.editTextGuesses);
+            TextView builder = (TextView) findViewById(R.id.textViewCorrect);
+        if(inputBox.getText().length()>0) {
+            char letter = inputBox.getText().charAt(0);
+            inputBox.setText("");
+
+
+            if (currentWord.charAt(guessBuilder.length()) == letter) {
+                guessBuilder += letter;
+                builder.setText(guessBuilder.toUpperCase());
+                if (currentWord.equals(guessBuilder)) {
+                    //setNextWord();
+                    displayResult();
+                }
+            } else {
+                guessCounter++;
+                updateCounter();
+            }
+        }
+    }
+
+    private void setNextWord(){
+        int index = random.nextInt(secretWords.size());
+        currentWord = secretWords.get(index);
+        TextView scrambleView = (TextView) findViewById(R.id.textViewScrambled);
+        scrambleView.setText(scrambleWord(currentWord).toUpperCase());
+        guessBuilder = "";
+        guessCounter = 0;
+        updateCounter();
+
+    }
+
+    private String scrambleWord(String word){
+
+        String shuffledWord = "";
+        ArrayList<String> splitWord = new ArrayList(Arrays.asList(word.split("")));
+        Collections.shuffle(splitWord);
+        for (String c : splitWord){
+            shuffledWord += c;
+        }
+        return shuffledWord;
+
+    }
+
+    private void displayResult(){
+        TextView outputMessage = (TextView) findViewById(R.id.instructionsTextView);
+        outputMessage.setText("You correctly deciphered " + currentWord.toUpperCase() + ". You made " + guessCounter + " mistakes.");
+    }
+
+    private void updateCounter(){
+        TextView guesses = (TextView) findViewById(R.id.textViewGuessCounter);
+        guesses.setText("" + guessCounter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_crypto_logic, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
